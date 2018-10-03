997e8505596ca66e84d844c4b9d3d0dde106deac
ArvindKumar1996 <arvindkumarcs1996@gmail.com>
2018-10-03 21:56:14 +0530

    First commit

---
+++ task/evenodd.js
@@ -0,0 +1,14 @@
+class EvenOdd
+{
+	constructor(h,w)
+	{
+		alert("Height = "+h+" and Width = "+w);
+	}
+checkEvenOdd(num)
+{
+if(num%2==0)
+alert(num+" is a even number")
+else
+alert(num+" is a odd number");
+}
+}
